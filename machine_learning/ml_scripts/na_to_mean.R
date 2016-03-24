b3f67ae8cccd974e84a329147aeef200670054d2
cjmadubata <cjm2200@columbia.edu>
2016-03-24 09:56:56 -0400

    rename machine_learning/ml_scripts/na_minus1.R to na_to_mean.R, improve descr.

--- machine_learning/ml_scripts/na_to_mean.R
+++ machine_learning/ml_scripts/na_to_mean.R
@@ -1,5 +1,6 @@
 na_to_mean = function(x) {
-  # This function converts a vector into numbers, and the resulting NAs into -1
+  # This function converts a vector into numbers, 
+  # and the resulting NAs into the mean of the present values 
   x = as.numeric(x)
   y = as.numeric(x[x!=-1])
   new = mean(y, na.rm = TRUE)
