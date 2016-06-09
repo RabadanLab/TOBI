833de6916a1e656323e7d9c8b9196186f26faecd
timchu90 <tim.chu90@gmail.com>
2016-06-09 15:52:58 -0400

    parallelized for each sample. uses multi-threading
    have not tested yet on large number of threads.

--- TOBIvaf.py
+++ TOBIvaf.py
@@ -8,6 +8,7 @@ import varCall_filtering.scripts.vcf2report as vcf2report
 import varCall_filtering.scripts.parse_tsv as parse_tsv
 import sys
 
+
 def get_arg():
     #Get Arguments
 
@@ -104,8 +105,7 @@ def get_arg():
     
     #arguments for merge
     parser.add_argument(
-        "--mergename", 
-        required=True, 
+        "--mergename",  
         help="[REQUIRED - MERGE] Name for final merged file"
         )
     
@@ -137,9 +137,9 @@ def main():
 
         if not os.path.exists(args.output+"/vcfcall"):
             os.makedirs(args.output + "/vcfcall/logs") 
-                
-        vcf_call(input_filenames,args)
-        #set inputdir as vcf_call's output
+        
+        helpers.multithread(vcf_call,args,input_filenames)
+        
         args.inputdir = args.output +"/vcfcall"
 
     #annotate
@@ -149,8 +149,8 @@ def main():
         #for each case name/bam file, make output directories
         if not os.path.exists(args.output+"/annotate"):
             os.makedirs(args.output + "/annotate/logs")
-                
-        annotate(input_filenames, args)
+        
+        helpers.multithread(annotate,args,input_filenames)
         #set inputdir as annotate's output
         args.inputdir = args.output +"/annotate"
     
@@ -161,12 +161,14 @@ def main():
         #for each case name/bam file, make output directories
         if not os.path.exists(args.output+"/filter"):
             os.makedirs(args.output + "/filter/logs")
-        filter_vcf(input_filenames, args)
+            
+        helpers.multithread(filter_vcf,args,input_filenames)
         #set inputdir as filter's output
         args.inputdir = args.output +"/filter"
     
     #merge
     if "M" in args.steps or "m" in args.steps:
+        helpers.check_merge_args(args)
         input_filenames = helpers.get_filenames(args.inputdir,"tsv")
         if len(input_filenames) <= 1:
             sys.exit("[ERROR] One or less files to merge.")
@@ -175,107 +177,113 @@ def main():
         merge_tsv(input_filenames,args)
         
 
-def vcf_call(input_filenames, args):
+def vcf_call(case_name, args):
     source_dir = os.path.dirname(os.path.realpath(__file__))
-    for case_name in input_filenames:
-        #run mpileup
-        helpers.runShellCmd(helpers.mpileup_cmdgen(args,case_name,source_dir))
-        
-        #concat raw_n.vcf files into new file
-        helpers.runShellCmd(helpers.vcf_concat_cmdgen(args,case_name))
-        
-        #sort vcf file
-        helpers.runShellCmd(
-                "vcf-sort -c " + args.output+"/vcfcall/"+case_name + ".vcf  > " 
-                + args.output+"/vcfcall/"+case_name+".sorted.vcf")
-        
-        #hacky way to create temp file fix this later
-        os.remove(args.output+"/vcfcall/"+case_name+".vcf")
-        os.rename(args.output+"/vcfcall/"+case_name+".sorted.vcf",
-                  args.output+"/vcfcall/"+case_name+".vcf")
-        #purge raw_n.vf files
-        if args.cleanup:
-            helpers.purge(args.output+"/vcfcall", "raw_\d*\.vcf")
+    #run mpileup
+    helpers.runShellCmd(helpers.mpileup_cmdgen(args,case_name,source_dir))
+    
+    #concat raw_n.vcf files into new file
+    helpers.runShellCmd(helpers.vcf_concat_cmdgen(args,case_name))
+    
+    #sort vcf file
+    helpers.runShellCmd(
+            "vcf-sort -c " + args.output+"/vcfcall/"+case_name + ".vcf  > " 
+            + args.output+"/vcfcall/"+case_name+".sorted.vcf")
+    
+    #hacky way to create temp file fix this later
+    os.remove(args.output+"/vcfcall/"+case_name+".vcf")
+    os.rename(args.output+"/vcfcall/"+case_name+".sorted.vcf",
+              args.output+"/vcfcall/"+case_name+".vcf")
+    #purge raw_n.vf files
+    if args.cleanup:
+        helpers.purge(args.output+"/vcfcall/"+case_name, "raw_\d*\.vcf")
 
-def annotate(input_filenames, args):
+def annotate(case_name, args):
     source_dir = os.path.dirname(os.path.realpath(__file__))
     annovcf = args.annovcf.replace("\n","").split(',')
-    dbnsfp_header="SIFT_score,SIFT_converted_rankscore,SIFT_pred,Polyphen2_HDIV_score,Polyphen2_HDIV_rankscore,Polyphen2_HDIV_pred,Polyphen2_HVAR_score,Polyphen2_HVAR_rankscore,Polyphen2_HVAR_pred,LRT_score,LRT_converted_rankscore,LRT_pred,MutationTaster_score,MutationTaster_converted_rankscore,MutationTaster_pred,MutationAssessor_score,MutationAssessor_rankscore,MutationAssessor_pred,FATHMM_score,FATHMM_rankscore,FATHMM_pred,RadialSVM_score,RadialSVM_rankscore,RadialSVM_pred,LR_score,LR_rankscore,LR_pred,Reliability_index,CADD_raw,CADD_raw_rankscore,CADD_phred,GERP++_NR,GERP++_RS,GERP++_RS_rankscore,phyloP46way_primate,phyloP46way_primate_rankscore,phyloP46way_placental,phyloP46way_placental_rankscore,phyloP100way_vertebrate,phyloP100way_vertebrate_rankscore,phastCons46way_primate,phastCons46way_primate_rankscore,phastCons46way_placental,phastCons46way_placental_rankscore,phastCons100way_vertebrate,phastCons100way_vertebrate_rankscore,SiPhy_29way_pi,SiPhy_29way_logOdds,SiPhy_29way_logOdds_rankscore,LRT_Omega,UniSNP_ids,1000Gp1_AC,1000Gp1_AF,1000Gp1_AFR_AC,1000Gp1_AFR_AF,1000Gp1_EUR_AC,1000Gp1_EUR_AF,1000Gp1_AMR_AC,1000Gp1_AMR_AF,1000Gp1_ASN_AC,1000Gp1_ASN_AF,ESP6500_AA_AF,ESP6500_EA_AF"
-    for case_name in input_filenames:      
-        if args.cluster == "hpc":
-            #snpEff annotate
-            helpers.runShellCmd(helpers.snpeff_cmdgen(args,case_name)) 
-            #snpSIFT annotate for each vcf provided
-            for vcf in annovcf:
-                helpers.runShellCmd(helpers.snpsift_cmdgen(args,case_name,vcf))
-                #hacky way to create & replace temp file. fix this later
-                os.remove(args.output+"/annotate/"+case_name+".eff.vcf")
-                os.rename(args.output+"/annotate/"+case_name+".eff.vcf.tmp",
-                          args.output+"/annotate/"+case_name+".eff.vcf"
-                          )
-            #snpSIFT dbnsfp
-            helpers.runShellCmd(
-                helpers.snpdbnsfp_cmdgen(args,case_name,args.dbnsfp,dbnsfp_header)
-                )
-            if args.cleanup:
-                os.remove(args.output+"/annotate/"+case_name+".eff.vcf")
-                
-            #split effects into one effect per line
-            helpers.runShellCmd(helpers.oneEff_cmdgen(args,case_name,source_dir))
+    dbnsfp_header="SIFT_score,SIFT_converted_rankscore,SIFT_pred,Polyphen2_HDIV_score,Polyphen2_HDIV_rankscore,Polyphen2_HDIV_pred,Polyphen2_HVAR_score,Polyphen2_HVAR_rankscore,Polyphen2_HVAR_pred,LRT_score,LRT_converted_rankscore,LRT_pred,MutationTaster_score,MutationTaster_converted_rankscore,MutationTaster_pred,MutationAssessor_score,MutationAssessor_rankscore,MutationAssessor_pred,FATHMM_score,FATHMM_rankscore,FATHMM_pred,RadialSVM_score,RadialSVM_rankscore,RadialSVM_pred,LR_score,LR_rankscore,LR_pred,Reliability_index,CADD_raw,CADD_raw_rankscore,CADD_phred,GERP++_NR,GERP++_RS,GERP++_RS_rankscore,phyloP46way_primate,phyloP46way_primate_rankscore,phyloP46way_placental,phyloP46way_placental_rankscore,phyloP100way_vertebrate,phyloP100way_vertebrate_rankscore,phastCons46way_primate,phastCons46way_primate_rankscore,phastCons46way_placental,phastCons46way_placental_rankscore,phastCons100way_vertebrate,phastCons100way_vertebrate_rankscore,SiPhy_29way_pi,SiPhy_29way_logOdds,SiPhy_29way_logOdds_rankscore,LRT_Omega,UniSNP_ids,1000Gp1_AC,1000Gp1_AF,1000Gp1_AFR_AC,1000Gp1_AFR_AF,1000Gp1_EUR_AC,1000Gp1_EUR_AF,1000Gp1_AMR_AC,1000Gp1_AMR_AF,1000Gp1_ASN_AC,1000Gp1_ASN_AF,ESP6500_AA_AF,ESP6500_EA_AF" 
+    if args.cluster == "hpc":
+        #snpEff annotate
+        helpers.runShellCmd(helpers.snpeff_cmdgen(args,case_name)) 
+        #snpSIFT annotate for each vcf provided
+        for vcf in annovcf:
+            helpers.runShellCmd(helpers.snpsift_cmdgen(args,case_name,vcf))
+            #hacky way to create & replace temp file. fix this later
+            os.remove(args.output+"/annotate/"+case_name+".eff.vcf")
+            os.rename(args.output+"/annotate/"+case_name+".eff.vcf.tmp",
+                      args.output+"/annotate/"+case_name+".eff.vcf"
+                      )
+        #snpSIFT dbnsfp
+        helpers.runShellCmd(
+            helpers.snpdbnsfp_cmdgen(args,case_name,args.dbnsfp,dbnsfp_header)
+            )
+        if args.cleanup:
+            os.remove(args.output+"/annotate/"+case_name+".eff.vcf")
             
-            if args.cleanup:
-                os.remove(args.output+"/annotate/"+case_name+".eff.all.vcf")
+        #split effects into one effect per line
+        helpers.runShellCmd(helpers.oneEff_cmdgen(args,case_name,source_dir))
+        
+        if args.cleanup:
+            os.remove(args.output+"/annotate/"+case_name+".eff.all.vcf")
 
-def filter_vcf(input_filenames,args):
+def filter_vcf(case_name,args):
+    if args.debug:
+        print("[Preprocessing file]")
     source_dir = os.path.dirname(os.path.realpath(__file__))
     #read in file
-    for case_name in input_filenames:
-        inputfile = open(args.inputdir+"/"+case_name+".vcf","r")
-        case_file = inputfile.read()
-        inputfile.close()
-        #find GERP++ and replace with GERP
-        case_file = case_file.replace('GERP++','GERP')
-        #vcf2report
-        case_file = vcf2report.convert(case_file)
-        #parse_tsv case_name
-        case_file = parse_tsv.convert(case_file,case_name)
-        #get rid of # and ' characters
-        case_file = case_file.replace("#","")
-        case_file = case_file.replace("'","")
-        #write to new file
-        outputfile = open(args.output+"/filter/"+case_name+"_not_filt.tsv","w")
-        outputfile.write(case_file)
-        outputfile.close()
-        #apply R script filters
-        if args.vcftype == "default":
-            script = "/varCall_filtering/scripts/filter_indel_techn_biol.pediatric.R "
-        elif args.vcftype == "TCGA":
-            script ="/varCall_filtering/scripts/filter_indel_techn_biol.R "
-        else:
-            sys.exit("[ERROR]: Invalid '--vcftype' argument: " + args.vcftype)
-            
-        proc = subprocess.Popen(
-            "Rscript " + source_dir + script
-                + args.output+"/filter/"+case_name+"_not_filt.tsv "
-                + args.output+"/filter/"+case_name+"_filt_indel_techn_biol.tsv",
-            shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-            )
-        if args.debug:
-            while True:
-                nextline = proc.stdout.readline()
-                if nextline == '' and proc.poll() != None:
-                    break
-                sys.stdout.write(nextline)
-                sys.stdout.flush()
-            while True:
-                nextline = proc.stderr.readline()
-                if nextline == '' and proc.poll() != None:
-                    break
-                sys.stderr.write(nextline)
-                sys.stderr.flush()
-        proc.wait()
-        if args.cleanup:
-            os.remove(args.output+"/filter/"+case_name+"_not_filt.tsv")
+    inputfile = open(args.inputdir+"/"+case_name+".vcf","r")
+    case_file = inputfile.read()
+    inputfile.close()
+    #find GERP++ and replace with GERP
+    case_file = case_file.replace('GERP++','GERP')
+    #vcf2report
+    case_file = vcf2report.convert(case_file)
+    #parse_tsv case_name
+    case_file = parse_tsv.convert(case_file,case_name)
+    #get rid of # and ' characters
+    case_file = case_file.replace("#","")
+    case_file = case_file.replace("'","")
+    #write to new file
+    outputfile = open(args.output+"/filter/"+case_name+"_not_filt.tsv","w")
+    outputfile.write(case_file)
+    outputfile.close()
+    #apply R script filters
+    if args.vcftype == "default":
+        script = "/varCall_filtering/scripts/filter_indel_techn_biol.pediatric.R "
+    elif args.vcftype == "TCGA":
+        script ="/varCall_filtering/scripts/filter_indel_techn_biol.R "
+    else:
+        sys.exit("[ERROR]: Invalid '--vcftype' argument: " + args.vcftype)
+    
+    cmd = "qsub -V -b y -sync y -N " + case_name \
+        + " -l mem=10G,time=2:: -pe smp 2 " \
+        + "-e " + args.output + "/filter/logs/"+ case_name +".e " \
+        + "-o " + args.output+"/filter/logs/"+case_name+".o " \
+        +"Rscript " + source_dir + script \
+        + args.output+"/filter/"+case_name+"_not_filt.tsv " \
+        + args.output+"/filter/"+case_name+"_filt_indel_techn_biol.tsv" 
+    if args.debug:
+        print(cmd)        
+    proc = subprocess.Popen(
+        cmd,
+        shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+    if args.debug:
+        while True:
+            nextline = proc.stdout.readline()
+            if nextline == '' and proc.poll() != None:
+                break
+            sys.stdout.write(nextline)
+            sys.stdout.flush()
+        while True:
+            nextline = proc.stderr.readline()
+            if nextline == '' and proc.poll() != None:
+                break
+            sys.stderr.write(nextline)
+            sys.stderr.flush()
+    proc.wait()
+    if args.cleanup:
+        os.remove(args.output+"/filter/"+case_name+"_not_filt.tsv")
 
 def merge_tsv(input_filenames,args):
     first = True
