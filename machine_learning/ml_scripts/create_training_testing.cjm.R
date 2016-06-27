bbe6b633c2f0f6f902b9079718371bf922e01574
cjmadubata <cjm2200@columbia.edu>
2016-06-27 19:49:41 -0400

    add two scripts to perform train set size analysis

---
+++ machine_learning/ml_scripts/create_training_testing.cjm.R
@@ -0,0 +1,37 @@
+create_training_testing_no_path = function(data, p, param, j,
+                                   dbSNP_only = TRUE, indel = FALSE) {
+  #cat("- Reading file\n")
+  y = data
+  # remove the dbSNP only
+  if (dbSNP_only) y = y[y$id2 != 0, ]
+  
+  # divide the samples into training and testing sets
+  y_list = sort(unique(y$case))
+  
+  no_testing = FALSE
+  if (length(y_list) == p) no_testing = TRUE
+  
+  cat("- Training\n")
+  num=j
+  set.seed(num)
+  training_list = sample(y_list, p)
+  training = y[ y$case %in% training_list, ]
+  training$num_mut_gene = num_mut_gene(training)
+  training$case_mut_gene = case_mut_gene(training)
+  training = cleaning(training, param, indel)
+  
+  if (! no_testing) {
+    cat("- Testing\n")
+    testing_list = y_list[! y_list %in% training_list]
+    testing = y[ y$case %in% testing_list, ]
+    testing$num_mut_gene = num_mut_gene(testing)
+    testing$case_mut_gene = case_mut_gene(testing)
+    testing = cleaning(testing, param, indel)
+    
+    y = list(training, testing)
+  } else {
+    y = list(training, FALSE)
+  }
+  
+  return(y)
+}
